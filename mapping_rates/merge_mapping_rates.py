0bc8f5304146670225f7087faed0bfd7f508eeaf
Leah Kemp <leah.kemp@esr.cri.nz>
2022-02-23 12:27:23 +1300

    fix if else statement for creating mapping rates data

--- mapping_rates/merge_mapping_rates.py
+++ mapping_rates/merge_mapping_rates.py
@@ -109,14 +109,14 @@ if smrnaseq_results_dir != "none" and excerpt_results_dir != "none":
   merged_df = pd.concat([sample_names, df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14, df15, df16, df17], axis=1)
 
 # if the user has chosen to analyse only the smrnaseq pipeline outputs...
-if smrnaseq_results_dir != "none":
+if smrnaseq_results_dir != "none" and excerpt_results_dir == "none":
 
   # merge all the dataframes into a single dataframe
   # "axis=1" tells it to merge as columns
   merged_df = pd.concat([sample_names, df1, df9, df10, df11, df12, df13, df14, df15, df16, df17], axis=1)
 
 # if the user has chosen to analyse both pipeline outputs...
-if excerpt_results_dir != "none":
+if smrnaseq_results_dir == "none" and excerpt_results_dir != "none":
 
   # merge all the dataframes into a single dataframe
   # "axis=1" tells it to merge as columns
